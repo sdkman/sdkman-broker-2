1d82b2f549d5a6a5abf3448a89c6d642cbbb78d4
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-05-01 16:49:41 +0100

    refactor: replace direct MongoDB interactions with MongoTestListener in acceptance and integration tests

--- src/test/kotlin/io/sdkman/broker/acceptance/HealthCheckAcceptanceSpec.kt
+++ src/test/kotlin/io/sdkman/broker/acceptance/HealthCheckAcceptanceSpec.kt
@@ -1,65 +1,31 @@
 package io.sdkman.broker.acceptance
 
-import com.mongodb.MongoClient
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
 import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import io.sdkman.broker.module
-import org.bson.Document
-import org.testcontainers.containers.MongoDBContainer
-import org.testcontainers.utility.DockerImageName
+import io.sdkman.broker.test.MongoTestListener
 
 /**
  * Acceptance tests for the health check endpoint based on BDD scenarios.
  */
-//TODO: Use a MongoTestListener instead to hand off MongoDB interaction to the listener
- class HealthCheckAcceptanceSpec : ShouldSpec() {
-
-    private val mongoContainer = MongoDBContainer(DockerImageName.parse("mongo:3.2"))
-        .apply { start() }
+class HealthCheckAcceptanceSpec : ShouldSpec() {
     
-    private val mongoClient = MongoClient(
-        mongoContainer.host,
-        mongoContainer.firstMappedPort
-    )
-    
-    private val database = mongoClient.getDatabase("sdkman")
-    private val applicationCollection = database.getCollection("application")
-    
-    private fun resetDatabase() {
-        applicationCollection.drop()
-    }
-    
-    override suspend fun beforeTest(testCase: TestCase) {
-        resetDatabase()
-        
-        // Set MongoDB connection for tests (default)
-        System.setProperty("mongodb.uri", 
-            "mongodb://${mongoContainer.host}:${mongoContainer.firstMappedPort}")
-        System.setProperty("mongodb.database", "sdkman")
-    }
-    
-    override suspend fun afterSpec(spec: io.kotest.core.spec.Spec) {
-        mongoClient.close()
-        mongoContainer.stop()
+    override suspend fun beforeSpec(spec: io.kotest.core.spec.Spec) {
+        listeners(MongoTestListener)
     }
     
     init {
         should("return 200 OK when database is healthy") {
             // given: an initialised database
-            applicationCollection.insertOne(Document("alive", "OK"))
+            MongoTestListener.setupValidAppRecord()
             
             // when: a GET request is made for "/health/alive"
             testApplication {
                 application { module() }
-                
                 val response = client.get("/health/alive")
-                val responseBody = response.bodyAsText()
-                println("DEBUG - Response body: $responseBody")
                 
                 // then: the service response status is 200
                 response.status shouldBe HttpStatusCode.OK
@@ -72,10 +38,7 @@ import org.testcontainers.utility.DockerImageName
             // when: a GET request is made for "/health/alive"
             testApplication {
                 application { module() }
-                
                 val response = client.get("/health/alive")
-                val responseBody = response.bodyAsText()
-                println("DEBUG - Response body: $responseBody")
                 
                 // then: the service response status is 503
                 response.status shouldBe HttpStatusCode.ServiceUnavailable
@@ -84,16 +47,12 @@ import org.testcontainers.utility.DockerImageName
         
         should("return 503 Service Unavailable when database has invalid application record") {
             // given: an uninitialised database with an invalid application record
-            applicationCollection.insertOne(Document("alive", "NOT_OK"))
+            MongoTestListener.setupInvalidAppRecord()
             
             // when: a GET request is made for "/health/alive"
             testApplication {
                 application { module() }
-                
                 val response = client.get("/health/alive")
-                val responseBody = response.bodyAsText()
-                //TODO: remove this println
-                println("DEBUG - Response body: $responseBody")
                 
                 // then: the service response status is 503
                 response.status shouldBe HttpStatusCode.ServiceUnavailable
@@ -107,20 +66,11 @@ import org.testcontainers.utility.DockerImageName
             // when: a GET request is made for "/health/alive"
             testApplication {
                 application { module() }
-                
                 val response = client.get("/health/alive")
-                val responseBody = response.bodyAsText()
-                //TODO: remove this println
-                println("DEBUG - Response body: $responseBody")
                 
                 // then: the service response status is 503
                 response.status shouldBe HttpStatusCode.ServiceUnavailable
             }
-            
-            // Reset to valid connection for other tests
-            //TODO: Use a MongoTestListener instead to hand off MongoDB interaction to the listener
-            System.setProperty("mongodb.uri", 
-                "mongodb://${mongoContainer.host}:${mongoContainer.firstMappedPort}")
         }
     }
 } 
---
+++ src/test/kotlin/io/sdkman/broker/test/MongoTestListener.kt
@@ -0,0 +1,43 @@
+package io.sdkman.broker.test
+
+import com.mongodb.MongoClient
+import io.kotest.core.listeners.TestListener
+import io.kotest.core.test.TestCase
+import org.bson.Document
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.utility.DockerImageName
+
+/**
+ * Shared MongoDB test container listener for tests.
+ * Centralizes MongoDB setup, connection management and cleanup.
+ */
+object MongoTestListener : TestListener {
+    private val mongoContainer = MongoDBContainer(DockerImageName.parse("mongo:3.2"))
+        .apply { start() }
+    
+    val host: String = mongoContainer.host
+    val port: Int = mongoContainer.firstMappedPort
+    
+    val mongoClient = MongoClient(host, port)
+    val database = mongoClient.getDatabase("sdkman")
+    val applicationCollection = database.getCollection("application")
+    
+    fun resetDatabase() {
+        applicationCollection.drop()
+    }
+    
+    fun setupValidAppRecord() {
+        applicationCollection.insertOne(Document("alive", "OK"))
+    }
+    
+    fun setupInvalidAppRecord() {
+        applicationCollection.insertOne(Document("alive", "NOT_OK"))
+    }
+    
+    override suspend fun beforeTest(testCase: TestCase) {
+        resetDatabase()
+        System.setProperty("mongodb.uri", "mongodb://$host:$port")
+        System.setProperty("mongodb.database", "sdkman")
+    }
+    
+} 
