d19e25547cbeda3cd96034248f2ded6f0ea529b6
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-05-03 14:13:44 +0100

    refactor: enhance AppConfig to use default values and add config extension functions

--- src/main/kotlin/io/sdkman/broker/config/AppConfig.kt
+++ src/main/kotlin/io/sdkman/broker/config/AppConfig.kt
@@ -8,13 +8,13 @@ class AppConfig {
     private val config: Config = ConfigFactory.load()
 
     // MongoDB settings
-    val mongodbDatabase: String = config.getString("mongodb.database")
-    val mongodbHost: String = config.getString("mongodb.host")
-    val mongodbPort: String = config.getString("mongodb.port")
-    val mongodbUsername: Option<String> = Option.fromNullable(config.getString("mongodb.username"))
-    val mongodbPassword: Option<String> = Option.fromNullable(config.getString("mongodb.password"))
+    val mongodbDatabase: String = config.getStringOrDefault("mongodb.database", "sdkman")
+    val mongodbHost: String = config.getStringOrDefault("mongodb.host", "localhost")
+    val mongodbPort: String = config.getStringOrDefault("mongodb.port", "27017")
+    val mongodbUsername: Option<String> = config.getOptionString("mongodb.username")
+    val mongodbPassword: Option<String> = config.getOptionString("mongodb.password")
 
     // Server settings
-    val serverPort: Int = config.getInt("server.port")
-    val serverHost: String = config.getString("server.host")
+    val serverPort: Int = config.getIntOrDefault("server.port", 8080)
+    val serverHost: String = config.getStringOrDefault("server.host", "0.0.0.0")
 }
---
+++ src/main/kotlin/io/sdkman/broker/config/ConfigExtensions.kt
@@ -0,0 +1,32 @@
+package io.sdkman.broker.config
+
+import arrow.core.Either
+import arrow.core.Option
+import arrow.core.getOrElse
+import arrow.core.none
+import com.typesafe.config.Config
+
+/**
+ * Get a string value from config with a default fallback
+ */
+fun Config.getStringOrDefault(path: String, default: String): String =
+    Either.catch { 
+        if (hasPath(path)) getString(path) else default 
+    }.getOrElse { default }
+
+/**
+ * Get an int value from config with a default fallback
+ */
+fun Config.getIntOrDefault(path: String, default: Int): Int =
+    Either.catch { 
+        if (hasPath(path)) getInt(path) else default 
+    }.getOrElse { default }
+
+/**
+ * Get an optional string value from config
+ */
+fun Config.getOptionString(path: String): Option<String> =
+    Either.catch {
+        if (!hasPath(path) || getIsNull(path)) none()
+        else Option.fromNullable(getString(path))
+    }.getOrElse { none() } 
