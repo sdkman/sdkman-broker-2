92b730f77139354ae5eb666346ebeea0863bc24e
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-05-05 11:45:11 +0100

    fix: attempt different hostname for tests for CI

--- src/main/kotlin/io/sdkman/broker/config/AppConfig.kt
+++ src/main/kotlin/io/sdkman/broker/config/AppConfig.kt
@@ -9,12 +9,12 @@ class AppConfig {
 
     // MongoDB settings
     val mongodbDatabase: String = config.getStringOrDefault("mongodb.database", "sdkman")
-    val mongodbHost: String = config.getStringOrDefault("mongodb.host", "localhost")
+    val mongodbHost: String = config.getStringOrDefault("mongodb.host", "127.0.0.1")
     val mongodbPort: String = config.getStringOrDefault("mongodb.port", "27017")
     val mongodbUsername: Option<String> = config.getOptionString("mongodb.username")
     val mongodbPassword: Option<String> = config.getOptionString("mongodb.password")
 
     // Server settings
     val serverPort: Int = config.getIntOrDefault("server.port", 8080)
-    val serverHost: String = config.getStringOrDefault("server.host", "localhost")
+    val serverHost: String = config.getStringOrDefault("server.host", "127.0.0.1")
 }
