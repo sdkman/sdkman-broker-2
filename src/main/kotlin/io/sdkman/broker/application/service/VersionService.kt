e3ce6f4809f07aaa31724ef90309baa11a81414b
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-07-20 00:15:08 +0100

    refactor: improve VersionService implementation

--- src/main/kotlin/io/sdkman/broker/application/service/VersionService.kt
+++ src/main/kotlin/io/sdkman/broker/application/service/VersionService.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import arrow.core.Option
 import arrow.core.flatMap
 import arrow.core.left
+import arrow.core.raise.either
 import arrow.core.right
 import io.sdkman.broker.adapter.primary.rest.AuditContext
 import io.sdkman.broker.domain.model.ArchiveType
@@ -13,9 +14,6 @@ import io.sdkman.broker.domain.model.Version
 import io.sdkman.broker.domain.model.VersionError
 import io.sdkman.broker.domain.repository.AuditRepository
 import io.sdkman.broker.domain.repository.VersionRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import org.slf4j.LoggerFactory
 import java.util.UUID
@@ -63,7 +61,6 @@ class VersionServiceImpl(
     private val auditRepository: AuditRepository
 ) : VersionService {
     private val logger = LoggerFactory.getLogger(this::class.java)
-    private val auditScope = CoroutineScope(Dispatchers.IO)
 
     override fun downloadVersion(
         candidate: String,
@@ -71,30 +68,24 @@ class VersionServiceImpl(
         platformCode: String,
         auditContext: AuditContext
     ): Either<VersionError, DownloadInfo> =
-        Platform.fromCode(platformCode)
-            .toEither { VersionError.InvalidPlatform(platformCode) }
-            .flatMap { platform ->
-                findVersionWithFallback(candidate, version, platform.persistentId)
-                    .map { versionEntity ->
-                        val checksumHeaders =
-                            versionEntity.checksums.mapKeys { (algorithm, _) ->
-                                "X-Sdkman-Checksum-${algorithm.uppercase()}"
-                            }
-                        val archiveType = ArchiveType.fromUrl(versionEntity.url).value
-
-                        val actualDist = versionEntity.resolveActualDistribution(platform)
-
-                        createAuditEntry(
-                            AuditCommand(candidate, version, platform, actualDist, versionEntity, auditContext)
-                        )
-
-                        DownloadInfo(
-                            redirectUrl = versionEntity.url,
-                            checksumHeaders = checksumHeaders,
-                            archiveType = archiveType
-                        )
-                    }
-            }
+        either<VersionError, DownloadInfo> {
+            val platform =
+                Platform.fromCode(platformCode).toEither { VersionError.InvalidPlatform(platformCode) }.bind()
+            val versionEntity = findVersionWithFallback(candidate, version, platform.persistentId).bind()
+            val checksumHeaders =
+                versionEntity.checksums.mapKeys { (algorithm, _) ->
+                    "X-Sdkman-Checksum-${algorithm.uppercase()}"
+                }
+            val archiveType = ArchiveType.fromUrl(versionEntity.url).value
+            val actualDist = versionEntity.resolveActualDistribution(platform)
+            createAuditEntry(AuditCommand(candidate, version, platform, actualDist, versionEntity, auditContext))
+            val downloadInfo = DownloadInfo(
+                redirectUrl = versionEntity.url,
+                checksumHeaders = checksumHeaders,
+                archiveType = archiveType
+            )
+            return downloadInfo.right()
+        }
 
     private fun findVersionWithFallback(
         candidate: String,
@@ -117,16 +108,13 @@ class VersionServiceImpl(
                 )
             }
 
-    private fun createAuditEntry(auditCommand: AuditCommand) {
-        auditScope.launch {
-            auditRepository.save(auditCommand.toAudit())
-                .mapLeft { failure ->
-                    logger.error(
-                        "Failed to save audit entry for download: {}",
-                        failure.exception.message,
-                        failure.exception
-                    )
-                }
-        }
-    }
+    private fun createAuditEntry(auditCommand: AuditCommand): Either<Unit, Unit> =
+        auditRepository.save(auditCommand.toAudit())
+            .mapLeft { failure ->
+                logger.error(
+                    "Failed to save audit entry for download: {}",
+                    failure.exception.message,
+                    failure.exception
+                )
+            }
 }
