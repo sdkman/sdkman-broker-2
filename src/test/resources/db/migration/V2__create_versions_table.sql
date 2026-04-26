1ef7a1a93ed894f3fa68d71b3d125d5c77d30840
Marco Vermeulen <vermeulen.mp@gmail.com>
2026-04-26 00:08:55 +0000

    feat: add versions table Flyway migration for tests

---
+++ src/test/resources/db/migration/V2__create_versions_table.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS versions;
+
+CREATE TABLE versions (
+    id              SERIAL    PRIMARY KEY,
+    candidate       TEXT      NOT NULL,
+    version         TEXT      NOT NULL,
+    distribution    TEXT          NULL,
+    platform        TEXT      NOT NULL,
+    visible         BOOLEAN   NOT NULL,
+    url             TEXT      NOT NULL,
+    md5_sum         TEXT          NULL,
+    sha_256_sum     TEXT          NULL,
+    sha_512_sum     TEXT          NULL,
+    created_at      TIMESTAMP     NULL DEFAULT now(),
+    last_updated_at TIMESTAMP     NULL DEFAULT now(),
+    UNIQUE (candidate, version, distribution, platform)
+);
